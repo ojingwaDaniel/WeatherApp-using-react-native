a5f97fe51dad5fab3ad824b43e1a9b9f83a00085
ojingwaDaniel <ojingwadanny@gmail.com>
2024-06-16 22:14:30 +0100

    added some changes

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, StyleSheet } from 'react-native'
+import CurrentWeather from "./src/components/CurrentWeather";
+const App = () => {
+  return (
+    <View style = {styles.container}>
+      <CurrentWeather/>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+
+
+  }
+
+})
+export default App
---
+++ src/components/UpcomingWeather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+const UpcomingWeather = () => {
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
+export default UpcomingWeather
